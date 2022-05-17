e54b52590ff803a0040472a731310c98e0549cdd
Andrew Litvinov <lita.study@bk.ru>
2022-05-17 18:17:25 +0300

    feat: task 01, 02 done

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = require('process');
+const uri = path.resolve(__dirname, 'text.txt');
+
+const readStrm = fs.createReadStream(uri);
+readStrm.setEncoding('utf-8');
+readStrm.read();
+readStrm.on('data', (chunk) =>{
+  stdout.write(chunk);
+} );
---
+++ 02-write-file/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = require('process');
+const uri = path.resolve(__dirname, 'text.txt');
+
+const writeStream = fs.createWriteStream(uri);
+writeStream.setDefaultEncoding('utf-8');
+stdout.write('let enter some text, then press Ctrl+C:');
+stdin.read();
+stdin.on('data',(chunk)=>{
+  writeStream.write(chunk);
+});
