c1da4ea286966ca2a3fc17a822673bf96391f029
Andrew Litvinov <lita.study@bk.ru>
2022-05-24 15:32:56 +0300

    fix: finishing message at 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,11 +1,19 @@
 const fs = require('fs');
 const path = require('path');
-const { stdin } = require('process');
+const process = require('process');
+const readline = require('node:readline');
+const inOut = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 const uri = path.resolve(__dirname, 'text.txt');
 
 const writeStream = fs.createWriteStream(uri, { 'encoding': 'utf-8' });
 console.log('let enter some text, then press Ctrl+C:', '\n\r');
-stdin.read();
-stdin.on('data', (chunk) => {
+process.stdin.read();
+process.stdin.on('data', (chunk) => {
   writeStream.write(chunk);
 });
+process.on('exit', ()=>console.log('File creatde ', uri));
+
